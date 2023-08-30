b46f98345fc94ed896814f61b37f46c7271a24fd
Iliyan Angelov <focusworking@gmail.com>
2023-08-30 22:40:33 +0200

    save

---
+++ src/Ardalist.Result.AzureFunctions/HttpRequestDataExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Ardalis.Result;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Threading.Tasks;
+
+namespace Ardalist.Result.AzureFunctions;
+
+public static partial class ResultExtensions
+{
+
+    public static HttpResponseData ToHttpRequestData<T>(this HttpRequestData req, Result<T> result)
+    {
+        return null;
+    }
+
+    public static HttpResponseData ToHttpRequestData(this HttpRequestData req, Ardalis.Result.Result result)
+    {
+        return null;
+    }
+
+    public static HttpResponseData ToHttpRequestData<T>(this Result<T> result, HttpRequestData req)
+    {
+        return null;
+    }
+
+    public static HttpResponseData ToHttpRequestData(this Ardalis.Result.Result result, HttpRequestData req)
+    {
+        return null;
+    }
+
+    internal static async Task<HttpResponseData> ToHttpRequestDataAsync(this HttpRequestData req, IResult result)
+    {
+        var response = req.CreateResponse(result.Status.ToHttpStatusResult());
+        if(result.Errors is not null)
+        {
+            await response.WriteAsJsonAsync(result.Errors);
+        }
+        else if(result.ValidationErrors is not null)
+        {
+            await response.WriteAsJsonAsync(result.ValidationErrors);
+        }
+        else
+        {
+            await response.WriteAsJsonAsync(result.GetValue());
+        }
+        return response;
+    }
+}
---
+++ src/Ardalist.Result.AzureFunctions/ResultStatusExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.Result;
+using System;
+using System.Net;
+
+namespace Ardalist.Result.AzureFunctions;
+
+internal static class ResultStatusExtensions
+{
+    public static HttpStatusCode ToHttpStatusResult(this ResultStatus statusCode)
+    {
+        return statusCode switch
+        {
+            ResultStatus.Ok => HttpStatusCode.OK,
+            ResultStatus.Error => HttpStatusCode.UnprocessableEntity,
+            ResultStatus.Forbidden => HttpStatusCode.Forbidden,
+            ResultStatus.Unauthorized => HttpStatusCode.Unauthorized,
+            ResultStatus.Invalid => HttpStatusCode.BadRequest,
+            ResultStatus.NotFound => HttpStatusCode.NotFound,
+            ResultStatus.Conflict => HttpStatusCode.Conflict,
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
